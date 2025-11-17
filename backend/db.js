6e7b0e1d990de9cb50fe9663d5cae68cbf9f9826
himanshu <iamhimanshu7102@gmail.com>
2025-11-17 22:52:07 +0530

    Connecting to mongo, using mongodb

--- backend/db.js
+++ backend/db.js
@@ -8,7 +8,7 @@ export default async function run() {
         // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
         await mongoose.connect(uri, clientOptions);
         await mongoose.connection.db.admin().command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log("Successfully connected!");
     } finally {
         // Ensures that the client will close when you finish/error
         await mongoose.disconnect();
--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,39 @@
 import run from "./db.js";
-run().catch(console.dir);
+import express from "express";
+import cors from "cors";
+import User from "./models/User.js";
+
+const PORT = process.env.PORT;
+const API_PATH = process.env.BASE_PATH;
+const API_VERSION = process.env.API_VERSION;
+const API_URL = API_PATH + '/' + API_VERSION;
+
+const app = express();
+
+// use cors
+app.use(cors());
+//json
+app.use(express.json());
+
+app.get(API_URL + '/welcome', (req, res) => {
+    res.status(200).send({ message: 'Hello, Welcome' });
+});
+
+app.post(API_URL + '/registration', (req, res) => {
+    console.log(req.body);
+
+    // const { username, email, password } = req.body;
+    // /**
+    //  * Check if the user is already present
+    //  */
+    // const alreadyExists = User.find({ username: { $exists: true, $eq: username } });
+    // console.log(alreadyExists);
+
+    res.status(200).send({ userexists: 0 })
+});
+
+
+app.listen(PORT, () => {
+    run().catch(console.dir);
+    console.log(`Server started, visit: http://localhost:${PORT}/welcome`);
+});
