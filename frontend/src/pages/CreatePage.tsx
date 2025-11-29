88eaf65815466f73a3c0137ef0bf2f035c293cd0
himanshu <iamhimanshu7102@gmail.com>
2025-11-29 23:18:07 +0530

    Interface added for posting content

--- frontend/src/pages/CreatePage.tsx
+++ frontend/src/pages/CreatePage.tsx
@@ -1,5 +1,18 @@
-import { Box, Container, TextField } from "@mui/material";
+import styled from "@emotion/styled";
+import { CloudUploadTwoTone } from "@mui/icons-material";
+import { Box, Button, Container, TextField } from "@mui/material";
 import React from "react";
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
 
 const CreatePage: React.FC = () => {
     return (
@@ -12,15 +25,37 @@ const CreatePage: React.FC = () => {
                     fullWidth={true}
                 />
             </Box>
-            <Box>
+            <Box sx={{ marginBottom: 4 }}>
+                <Button
+                    component="label"
+                    role={undefined}
+                    variant="outlined"
+                    tabIndex={-1}
+                    fullWidth={true}
+                    startIcon={<CloudUploadTwoTone />}
+                >
+                    Upload Header Image
+                    <VisuallyHiddenInput
+                        type="file"
+                        onChange={(event) => console.log(event.target.files)}
+                    />
+                </Button>
+            </Box>
+            <Box sx={{ marginBottom: 4 }}>
                 <TextField
                     id="standard-multiline-flexible"
                     label="Content"
+                    required
                     multiline
-                    fullWidth={true}
+                    fullWidth
                     rows={10}
                     sx={{ minHeight: '200px' }}
                 />
+            </Box>
+            <Box sx={{ textAlign: 'right' }}>
+                <Button variant="outlined" size="medium">
+                    Submit
+                </Button>
             </Box>
         </Container>
     );
