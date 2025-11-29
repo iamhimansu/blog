b25b829e648f2bca6c5387e8942149521f237569
himanshu <iamhimanshu7102@gmail.com>
2025-11-29 23:49:42 +0530

    Can add post through interface

--- frontend/src/pages/CreatePage.tsx
+++ frontend/src/pages/CreatePage.tsx
@@ -1,61 +1,122 @@
 import styled from "@emotion/styled";
 import { CloudUploadTwoTone } from "@mui/icons-material";
-import { Box, Button, Container, TextField } from "@mui/material";
-import React from "react";
-const VisuallyHiddenInput = styled('input')({
-    clip: 'rect(0 0 0 0)',
-    clipPath: 'inset(50%)',
-    height: 1,
-    overflow: 'hidden',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    whiteSpace: 'nowrap',
-    width: 1,
-});
+import { Alert, Box, Button, Container, TextField } from "@mui/material";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import React, { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const CreatePage: React.FC = () => {
+    const navigate = useNavigate();
+
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const VisuallyHiddenInput = styled('input')({
+        clip: 'rect(0 0 0 0)',
+        clipPath: 'inset(50%)',
+        height: 1,
+        overflow: 'hidden',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        whiteSpace: 'nowrap',
+        width: 1,
+    });
+
+    interface CreateContentResponse {
+        message: string,
+        status: Number
+    }
+
+    interface CreateContentVariables {
+        title: string,
+        content: string
+    }
+
+    const createPageMutation = useMutation<CreateContentResponse, Error, CreateContentVariables>({
+        mutationFn: async (contentData) => {
+            const response = await axios.post('http://localhost:5001/api/v1/posts/create', contentData);
+            return response.data;
+        },
+        onError: (error) => {
+            console.log(error.message);
+        },
+        onSuccess: (data) => {
+            if (data.status === 200) {
+                // setTimeout(() => navigate('/create'), 2000);
+            }
+            throw new Error(data.message);
+        }
+    });
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        const contentData = (document.getElementById('content') as HTMLTextAreaElement)?.value || '';
+        setContent(contentData);
+        createPageMutation.mutate({
+            title, content
+        });
+    }
+
     return (
         <Container maxWidth="md" sx={{ padding: '2rem' }}>
-            <Box sx={{ marginBottom: 4 }}>
-                <TextField
-                    required
-                    id="outlined-required"
-                    label="Title"
-                    fullWidth={true}
-                />
-            </Box>
-            <Box sx={{ marginBottom: 4 }}>
-                <Button
-                    component="label"
-                    role={undefined}
-                    variant="outlined"
-                    tabIndex={-1}
-                    fullWidth={true}
-                    startIcon={<CloudUploadTwoTone />}
-                >
-                    Upload Header Image
-                    <VisuallyHiddenInput
-                        type="file"
-                        onChange={(event) => console.log(event.target.files)}
+
+            {createPageMutation.isError && (
+                <Alert severity="error" sx={{ mt: 2 }}>
+                    {createPageMutation.error.message}
+                </Alert>
+            )}
+            {createPageMutation.isPending && (
+                <Alert severity="info" sx={{ mt: 2 }}>
+                    Creating...
+                </Alert>
+            )}
+            {createPageMutation.isSuccess && (
+                <Alert severity="success" sx={{ mt: 2 }}>
+                    {createPageMutation.data.message}
+                </Alert>
+            )}
+            <Box component="form" onSubmit={handleSubmit}>
+                <Box sx={{ marginBottom: 4 }}>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Title"
+                        fullWidth
+                        onChange={(e) => setTitle(e.target.value)}
                     />
-                </Button>
-            </Box>
-            <Box sx={{ marginBottom: 4 }}>
-                <TextField
-                    id="standard-multiline-flexible"
-                    label="Content"
-                    required
-                    multiline
-                    fullWidth
-                    rows={10}
-                    sx={{ minHeight: '200px' }}
-                />
-            </Box>
-            <Box sx={{ textAlign: 'right' }}>
-                <Button variant="outlined" size="medium">
-                    Submit
-                </Button>
+                </Box>
+                <Box sx={{ marginBottom: 4 }}>
+                    <Button
+                        component="label"
+                        variant="outlined"
+                        tabIndex={-1}
+                        fullWidth={true}
+                        startIcon={<CloudUploadTwoTone />}
+                    >
+                        Upload Header Image
+                        <VisuallyHiddenInput
+                            type="file"
+                        />
+                    </Button>
+                </Box>
+                <Box sx={{ marginBottom: 4 }}>
+                    <TextField
+                        id="content"
+                        label="Content"
+                        required
+                        multiline
+                        fullWidth
+                        rows={10}
+                        sx={{ minHeight: '200px' }}
+                    />
+                </Box>
+                <Box sx={{ textAlign: 'right' }}>
+                    <Button variant="outlined" type="submit" size="medium">
+                        Submit
+                    </Button>
+                </Box>
             </Box>
         </Container>
     );
